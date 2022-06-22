bff586e7beb815540c68fadb19f442a68d5bc7a1
CelesTech03 <celeste.urena@stu.bmcc.cuny.edu>
2022-06-21 21:27:23 -0400

    added emotionalgifs part

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -4,21 +4,25 @@ const fetch = require('node-fetch')
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    // here's your boundary:
     const boundary = multipart.getBoundary(req.headers['content-type']);
   
-    // TODO: assign the body variable the correct value
+    // Assigns the body variable the correct value
     const body = req.body;
 
-    // parse the body
+    // parses the body
     const parts = multipart.Parse(body, boundary);
 
     // module.exports function
-    // analyze the image
+    // analyzes the image
     const result = await analyzeImage(parts[0].data);
+
+    let emotions = result[0].faceAttributes.emotion;
+    let objects = Object.values(emotions);
+    const main_emotion = Object.keys(emotions).find(key => emotions[key] === Math.max(...objects));
+
     context.res = {
         body: {
-            result
+            main_emotion
         }
     };
     console.log(result)
@@ -26,9 +30,10 @@ module.exports = async function (context, req) {
 }
 
 async function analyzeImage(img) {
-    const subscriptionKey = process.env.SUBSCRIPTIONKEY;
-    const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
-
+    //const subscriptionKey = process.env.SUBSCRIPTIONKEY;
+    const subscriptionKey = '3c5629de3c5b445a95e463c5a55a1f73';
+    //const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
+    const uriBase = 'https://celisfaceapi.cognitiveservices.azure.com/' + '/face/v1.0/detect';
     let params = new URLSearchParams({
         'returnFaceId': 'true',
         'returnFaceAttributes': 'emotion'  //FILL IN THIS LINE
